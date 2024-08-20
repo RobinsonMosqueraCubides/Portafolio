ed2da487ac0bf5679cc18cc2582106bdac7e0b59
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-08-20 10:49:42 -0500

    feat: :sparkles: Animacion de fondo
    Se crea la funcion y las animaciones del fondo

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  particlesJS('particles-js', {
+      "particles": {
+          "number": {
+              "value": 80
+          },
+          "size": {
+              "value": 3
+          },
+          "move": {
+              "enable": true,
+              "speed": 2
+          }
+      },
+      "interactivity": {
+          "events": {
+              "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+              }
+          }
+      }
+  });
+});
 const light = document.getElementById('light');
 
   document.addEventListener('mousemove', (e) => {
